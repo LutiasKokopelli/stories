4a23c1919023f8428179b2087534d5e0300381ec
LutiasKokopelli <94559567+LutiasKokopelli@users.noreply.github.com>
2021-11-18 07:02:46 +0100

    Delete screensize.php

--- css/screensize.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet"type="text/css"href="http://learnwhentoquit/css/css/CSS-main.css"media="screen">
-
-<link rel='stylesheet' media='screen and (max-width:  640px)' href='http://learnwhentoquit/css/css/CSS-phone.css' />
-<link rel='stylesheet' media='screen and (min-width:  641px) and (max-width: 1200px)' href='http://learnwhentoquit/css/css/CSS-medium-small.css' />
-<link rel='stylesheet' media='screen and (min-width: 1201px) and (max-width: 1650px)' href='http://learnwhentoquit/css/css/CSS-medium.css' />
-<link rel='stylesheet' media='screen and (min-width: 1651px)' href='http://learnwhentoquit/css/css/CSS-intended.css' />
