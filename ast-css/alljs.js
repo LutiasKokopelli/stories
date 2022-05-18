a086e30f744178d086acbec6742cc343c2a29013
LutiasKokopelli <94559567+LutiasKokopelli@users.noreply.github.com>
2022-05-19 01:07:23 +0200

    Add files via upload

--- ast-css/alljs.js
+++ ast-css/alljs.js
@@ -14,12 +14,16 @@ function ToOld(b2past,b2futu,divpas,divfut){
     bf=document.getElementById(b2futu);
     dp=document.getElementById(divpas);
     df=document.getElementById(divfut);
-    df.style.display="none";dp.style.display="block";bf.style.display="none";bp.style.display="block"
+    if((bf.style.display=="none")&&(bp.style.display=="none")){df.style.display="none";bf.style.display="none";dp.style.display="block";bp.style.display="none"}
+    else{df.style.display="none";bf.style.display="none";dp.style.display="block";bp.style.display="block"}
 }
 function ToNew(b2past,b2futu,divpas,divfut){
     bp=document.getElementById(b2past);
     bf=document.getElementById(b2futu);
     dp=document.getElementById(divpas);
     df=document.getElementById(divfut);
-    dp.style.display="none";df.style.display="block";bp.style.display="none";bf.style.display="block"
+    if((bf.style.display=="none")&&(bp.style.display=="none")){dp.style.display="none";bp.style.display="none";df.style.display="block";bf.style.display="none"}
+    else{dp.style.display="none";bp.style.display="none";df.style.display="block";bf.style.display="block"}
 }
+function PToOld(divpas,divfut){dp=document.getElementById(divpas);df=document.getElementById(divfut);df.style.display="none";dp.style.display="block"}
+function PToNew(divpas,divfut){dp=document.getElementById(divpas);df=document.getElementById(divfut);dp.style.display="none";df.style.display="block"}
