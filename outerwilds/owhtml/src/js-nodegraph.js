931853553fab7e2791f612be5768bef1abbfffb1
LutiasKokopelli <94559567+LutiasKokopelli@users.noreply.github.com>
2025-03-08 23:34:08 +0100

    Update js-nodegraph.js

--- outerwilds/owhtml/src/js-nodegraph.js
+++ outerwilds/owhtml/src/js-nodegraph.js
@@ -2792,9 +2792,11 @@ function wu(t, e, n = {
             }
         }
         if (v.rect && (c.fillStyle = "white", c.strokeStyle = "black", c.lineWidth = 1 / u.k, c.beginPath(), c.rect(v.rect.x, v.rect.y, v.rect.width, v.rect.height), c.fill(), c.stroke(), c.closePath()), v.arrow && (c.lineWidth = 1 / u.k, c.fillStyle = "white", c.beginPath(), c.moveTo(v.arrow.x[0], v.arrow.x[1]), c.lineTo(v.arrow.y[0], v.arrow.y[1]), c.lineTo(v.arrow.z[0], v.arrow.z[1]), c.fill(), c.closePath(), c.beginPath(), c.moveTo(v.arrow.z[0], v.arrow.z[1]), c.lineTo(v.arrow.x[0], v.arrow.x[1]), c.moveTo(v.arrow.z[0], v.arrow.z[1]), c.lineTo(v.arrow.y[0], v.arrow.y[1]), c.stroke(), c.closePath()), v.text) {
-            c.fillStyle = "black";c.fontVariantCaps="small-caps";
+            c.fillStyle = "black";
             let C = (($ = n.node) == null ? void 0 : $.tooltipFontSize) ?? 20;
-            c.font = `${C / u.k}px Consolas`, c.fillText(v.text.content, v.text.x, v.text.y);
+            c.font = `${C / u.k}px Consolas`,
+            c.fontVariantCaps="small-caps"
+            c.fillText(v.text.content, v.text.x, v.text.y);
         }
         c.restore();
     }
