057e3bdb1967d86764714731a1de0f00c39d1b06
LutiasKokopelli <94559567+LutiasKokopelli@users.noreply.github.com>
2025-03-08 23:00:32 +0100

    Update js-nodegraph.js

--- outerwilds/owhtml/src/js-nodegraph.js
+++ outerwilds/owhtml/src/js-nodegraph.js
@@ -2783,7 +2783,7 @@ function wu(t, e, n = {
             let O = R.label ?? ((k = n.node) == null ? void 0 : k.label);
             if (O) {
                 let K = ((A = n == null ? void 0 : n.node) == null ? void 0 : A.labelFontSize) ?? 14;
-                c.font = `${K}px Consolas`, c.fontVariantCaps="small-caps",c.textBaseline='middle'
+                c.font = `${K}px Consolas`, c.fontVariantCaps="small-caps",c.textBaseline='bottom'
 
                 c.strokeStyle="black",c.lineWidth=1,c.lineCap="round",c.lineJoin="round",
                 c.strokeText(typeof O=="function"?O(R,C):typeof O=="boolean"?R.id:O,R.x-w/2,R.y+w/2),
